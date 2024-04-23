a85812565be3fc1b6f67dc032118b956c057a359
Ivan Lev <fanatos@mail.ru>
2024-04-23 14:49:14 +0500

    feat: add Gravity UI theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
 import './index.css';
 
+// Gravity UI styles and themes
+import { ThemeProvider } from '@gravity-ui/uikit';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme="dark">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
