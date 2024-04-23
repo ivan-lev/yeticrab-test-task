217cb434cef4c193aaef25931eda18ba0b7a2ebf
Ivan Lev <fanatos@mail.ru>
2024-04-23 12:13:47 +0500

    feat: add records variable for sample rendering

---
+++ src/types/RecordStatus.ts
@@ -0,0 +1,1 @@
+export type RecordStatus = 'новая' | 'в работе' | 'завершено';
---
+++ src/variables/records.ts
@@ -0,0 +1,34 @@
+import { RecordElementType } from '../types/RecordElementType';
+
+export const records: RecordElementType[] = [
+  {
+    number: 121,
+    datetime: 1713853980663,
+    companyName: 'Компания Витязи',
+    clientName: 'Иванов Иван Иванович',
+    clientPhone: '+7-903-097-9332',
+    comment: '',
+    status: 'новая',
+    atiCode: 12345
+  },
+  {
+    number: 122,
+    datetime: 1713854980663,
+    companyName: 'ТрансКолсант',
+    clientName: 'Тарасенко Клим Петрович',
+    clientPhone: '+7-914-017-3352',
+    comment: 'Осторожно, хрупкое',
+    status: 'в работе',
+    atiCode: 13375
+  },
+  {
+    number: 123,
+    datetime: 1713864980663,
+    companyName: 'Наземные Линии',
+    clientName: 'Наземный Егор Сергеевич',
+    clientPhone: '+7-932-317-1754',
+    comment: 'Не кантовать',
+    status: 'завершено',
+    atiCode: 1437
+  }
+];
