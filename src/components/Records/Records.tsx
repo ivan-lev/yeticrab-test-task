45cf8b311b12e4db788aaf38901681010d8ee70a
Ivan Lev <fanatos@mail.ru>
2024-04-23 15:37:31 +0500

    feat: add initial Records component with table

--- src/components/Records/Records.tsx
+++ src/components/Records/Records.tsx
@@ -1,15 +1,69 @@
 import './Records.scss';
 
-import RecordElement from '../RecordElement/RecordElement';
+//Components
+import { Table, Link, withTableActions, TableDataItem } from '@gravity-ui/uikit';
 
-import { records } from '../../variables/records';
+//utils and variables
+import { getDate } from '../../utils/getDate';
+import { getAtiLink } from '../../utils/getAtiLink';
 
-export default function Records(): JSX.Element {
-  return (
-    <ul>
-      {records.map(record => (
-        <RecordElement key={record.number} record={record} />
-      ))}
-    </ul>
-  );
+//Types
+import { tableColumnsEnum } from '../../types/tableColumnsType';
+import { RecordElementType } from '../../types/RecordElementType';
+
+export default function Records({ records }: { records: RecordElementType[] }): JSX.Element {
+  const MyTable = withTableActions(Table);
+
+  const columns = [
+    { id: tableColumnsEnum.Number },
+    { id: tableColumnsEnum.DateTime },
+    { id: tableColumnsEnum.ClientsFirm },
+    { id: tableColumnsEnum.Shipper },
+    { id: tableColumnsEnum.ShipperNumber },
+    { id: tableColumnsEnum.Comment },
+    { id: tableColumnsEnum.Status },
+    { id: tableColumnsEnum.AtiCode }
+  ];
+
+  const data = records.map(record => {
+    return {
+      id: record.number,
+      [tableColumnsEnum.Number]: record.number,
+      [tableColumnsEnum.DateTime]: getDate(record.datetime),
+      [tableColumnsEnum.ClientsFirm]: record.companyName,
+      [tableColumnsEnum.Shipper]: record.clientName,
+      [tableColumnsEnum.ShipperNumber]: record.phone,
+      [tableColumnsEnum.Comment]: record.comment,
+      [tableColumnsEnum.Status]: record.status,
+      [tableColumnsEnum.AtiCode]: (
+        <Link href={getAtiLink(record.atiCode)} target="_blank">
+          {record.atiCode}
+        </Link>
+      )
+    };
+  });
+
+  const getRowActions = () => {
+    return [
+      {
+        text: 'Print',
+        handler: (item: TableDataItem) => {
+          console.log(item);
+        }
+      },
+      {
+        text: 'Edit',
+        handler: (item: TableDataItem, index: number) => {
+          console.log({ item, index });
+        }
+      },
+      {
+        text: 'Remove',
+        handler: (item: TableDataItem, index: number) => {},
+        theme: 'danger'
+      }
+    ];
+  };
+
+  return <MyTable data={data} columns={columns} getRowActions={getRowActions} />;
 }
