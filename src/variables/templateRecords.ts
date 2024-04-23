32e39c9957280e93da76790c1d40e42f4831871b
Ivan Lev <fanatos@mail.ru>
2024-04-23 22:39:33 +0500

    feat: update template records and fields names

--- src/variables/templateRecords.ts
+++ src/variables/templateRecords.ts
@@ -4,9 +4,9 @@ export const templateRecords: RecordElementType[] = [
   {
     number: 121,
     datetime: '2024-04-21T17:45:23.123Z',
-    companyName: 'Компания Витязи',
-    clientName: 'Иванов Иван Иванович',
-    phone: '+7-903-097-9332',
+    clientsFirm: 'Компания Витязи',
+    shipperName: 'Иванов Иван Иванович',
+    shipperPhone: '+7-903-097-9332',
     comment: '',
     status: 'новая',
     atiCode: 12345
@@ -14,9 +14,9 @@ export const templateRecords: RecordElementType[] = [
   {
     number: 122,
     datetime: '2024-04-22T11:33:15.456Z',
-    companyName: 'ТрансКолсант',
-    clientName: 'Тарасенко Клим Петрович',
-    phone: '+7-914-017-3352',
+    clientsFirm: 'ТрансКолсант',
+    shipperName: 'Тарасенко Клим Петрович',
+    shipperPhone: '+7-914-017-3352',
     comment: 'Осторожно, хрупкое',
     status: 'в работе',
     atiCode: 13375
@@ -24,9 +24,59 @@ export const templateRecords: RecordElementType[] = [
   {
     number: 123,
     datetime: '2024-04-23T14:35:12.789Z',
-    companyName: 'Наземные Линии',
-    clientName: 'Наземный Егор Сергеевич',
-    phone: '+7-932-317-1754',
+    clientsFirm: 'Наземные Линии',
+    shipperName: 'Наземный Егор Сергеевич',
+    shipperPhone: '+7-932-317-1754',
+    comment: 'Не кантовать',
+    status: 'завершено',
+    atiCode: 1437
+  },
+  {
+    number: 124,
+    datetime: '2024-04-23T15:47:12.789Z',
+    clientsFirm: 'Белкины Пряники',
+    shipperName: 'Тарасов Святослав Михайлович',
+    shipperPhone: '+7-909-745-4564',
+    comment: 'Пищевые товары',
+    status: 'новая',
+    atiCode: 1420
+  },
+  {
+    number: 125,
+    datetime: '2024-04-23T14:35:12.789Z',
+    clientsFirm: 'Суздальский Хрусталь',
+    shipperName: 'Митрофанов Даниил Васильевич',
+    shipperPhone: '+7-911-300-4686',
+    comment: 'Очень хрупкий товар',
+    status: 'новая',
+    atiCode: 1438
+  },
+  {
+    number: 126,
+    datetime: '2024-04-21T17:45:23.123Z',
+    clientsFirm: 'Компания Витязи',
+    shipperName: 'Иванов Иван Иванович',
+    shipperPhone: '+7-903-097-9332',
+    comment: '',
+    status: 'новая',
+    atiCode: 12345
+  },
+  {
+    number: 127,
+    datetime: '2024-04-22T11:33:15.456Z',
+    clientsFirm: 'ТрансКолсант',
+    shipperName: 'Тарасенко Клим Петрович',
+    shipperPhone: '+7-914-017-3352',
+    comment: 'Осторожно, хрупкое',
+    status: 'в работе',
+    atiCode: 13375
+  },
+  {
+    number: 128,
+    datetime: '2024-04-23T14:35:12.789Z',
+    clientsFirm: 'Наземные Линии',
+    shipperName: 'Наземный Егор Сергеевич',
+    shipperPhone: '+7-932-317-1754',
     comment: 'Не кантовать',
     status: 'завершено',
     atiCode: 1437
