8fa057c6b51f8aa1c580d7de535cf0f97f4fb109
Ivan Lev <fanatos@mail.ru>
2024-04-23 14:38:12 +0500

    style: rename record to templateRecords

--- src/variables/templateRecords.ts
+++ src/variables/templateRecords.ts
@@ -1,32 +1,32 @@
 import { RecordElementType } from '../types/RecordElementType';
 
-export const records: RecordElementType[] = [
+export const templateRecords: RecordElementType[] = [
   {
     number: 121,
-    datetime: 1713853980663,
+    datetime: '2024-04-21T17:45:23.123Z',
     companyName: 'Компания Витязи',
     clientName: 'Иванов Иван Иванович',
-    clientPhone: '+7-903-097-9332',
+    phone: '+7-903-097-9332',
     comment: '',
     status: 'новая',
     atiCode: 12345
   },
   {
     number: 122,
-    datetime: 1713854980663,
+    datetime: '2024-04-22T11:33:15.456Z',
     companyName: 'ТрансКолсант',
     clientName: 'Тарасенко Клим Петрович',
-    clientPhone: '+7-914-017-3352',
+    phone: '+7-914-017-3352',
     comment: 'Осторожно, хрупкое',
     status: 'в работе',
     atiCode: 13375
   },
   {
     number: 123,
-    datetime: 1713864980663,
+    datetime: '2024-04-23T14:35:12.789Z',
     companyName: 'Наземные Линии',
     clientName: 'Наземный Егор Сергеевич',
-    clientPhone: '+7-932-317-1754',
+    phone: '+7-932-317-1754',
     comment: 'Не кантовать',
     status: 'завершено',
     atiCode: 1437
