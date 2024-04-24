2a410b4f84031d3ef7f74bd236122c05f639a4f5
Ivan Lev <fanatos@mail.ru>
2024-04-24 12:35:49 +0500

    refactor: return component's content in parent div block

--- src/components/RecordsCounter/RecordsCounter.tsx
+++ src/components/RecordsCounter/RecordsCounter.tsx
@@ -2,9 +2,9 @@ import { Text } from '@gravity-ui/uikit';
 
 export default function RecordsCounter({ amount }: { amount: number }): JSX.Element {
   return (
-    <>
+    <div>
       <Text color="secondary">Количество заявок: </Text>
       <Text color="brand">{amount}</Text>
-    </>
+    </div>
   );
 }
