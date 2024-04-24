9eef6c1c24e1a9715a23440fba7870bfd21bfec0
Ivan Lev <fanatos@mail.ru>
2024-04-24 12:46:46 +0500

    feat: add Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,40 @@
+import './Main.scss';
+
+// React components
+import { Dispatch, SetStateAction } from 'react';
+
+// Components
+import Records from '../Records/Records';
+
+// Gravity UI components
+import { Text } from '@gravity-ui/uikit';
+
+// Variables, types, interfaces
+import { RecordElementType } from '../../types/RecordElementType';
+
+export default function Main({
+  records,
+  setRecords,
+  isAdminMode,
+  latestRecordNumber,
+  setLatestRecordNumber
+}: {
+  records: RecordElementType[];
+  setRecords: Dispatch<SetStateAction<RecordElementType[]>>;
+  isAdminMode: boolean;
+  latestRecordNumber: number;
+  setLatestRecordNumber: Dispatch<SetStateAction<number>>;
+}): JSX.Element {
+  return (
+    <main className="content">
+      <Text variant="header-1">Текущие заявки</Text>
+      <Records
+        records={records}
+        setRecords={setRecords}
+        isAdminMode={isAdminMode}
+        latestRecordNumber={latestRecordNumber}
+        setLatestRecordNumber={setLatestRecordNumber}
+      />
+    </main>
+  );
+}
