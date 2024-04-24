9b36857310ae62982713904af8bc6198b101d5af
Ivan Lev <fanatos@mail.ru>
2024-04-24 12:35:04 +0500

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,41 @@
+import './Header.scss';
+
+//React components
+import { Dispatch, SetStateAction } from 'react';
+
+import { Button } from '@gravity-ui/uikit';
+
+export default function Header({
+  isAdminMode,
+  setIsAdminMode
+}: {
+  isAdminMode: boolean;
+  setIsAdminMode: Dispatch<SetStateAction<boolean>>;
+}): JSX.Element {
+  const changeAdminMode = () => {
+    setIsAdminMode(!isAdminMode);
+  };
+  return (
+    <header className="header">
+      {isAdminMode ? (
+        <Button
+          className="header__button"
+          view="outlined-success"
+          size="l"
+          onClick={changeAdminMode}
+        >
+          Перейти в режим просмотра
+        </Button>
+      ) : (
+        <Button
+          className="header__button"
+          view="outlined-action"
+          size="l"
+          onClick={changeAdminMode}
+        >
+          Перейти в режим администратора
+        </Button>
+      )}
+    </header>
+  );
+}
